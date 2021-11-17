396ce00736c17ea2a42ad9e01a2b18fae48138d8
Qsu <51903703+ykkaq@users.noreply.github.com>
2021-11-17 14:34:21 +0900

    image.load()に.convert()の追加 デバッグ用のテキスト表示の追加

--- homerun/homerun.py
+++ homerun/homerun.py
@@ -1,17 +1,26 @@
+# -*- coding: utf8 -*-
+
 import pygame
 from pygame.locals import *
 import sys
 import os
+import test1
 
-imgGround = pygame.image.load("pct/ground.png")
-imgPitcher = pygame.image.load("pct/baseball_pitcher_woman_fix.png")
+# 初期化とか
+width = 1200 #画面横
+height = 900 #画面縦
+screen = pygame.display.set_mode((width,height))  #ウィンドウの大きさ指定
+
+# 画像読み込み
+imgGround = pygame.image.load("pct/ground.png").convert()
+imgPitcher = pygame.image.load("pct/baseball_pitcher_woman_fix.png").convert_alpha()
 imgPitcher = pygame.transform.smoothscale(imgPitcher, (122, 150)) #orgnSize = 732*800
-imgBall = pygame.image.load("pct/baseball_ball.png")
+imgBall = pygame.image.load("pct/baseball_ball.png").convert_alpha()
 imgBall = pygame.transform.smoothscale(imgBall, (20, 20))
-imgBat = pygame.image.load("pct/sport_baseball_bat.png")
+imgBat = pygame.image.load("pct/sport_baseball_bat.png").convert_alpha()
 imgBat = pygame.transform.rotozoom(imgBat, -50 ,0.22) #orgnSize = 479*610
 
-
+# 座標クラス
 class coordinate:
   def __init__(self,x,y):
     self.x = x
@@ -20,6 +29,7 @@ class coordinate:
   def add(self,num):
     self+=num
 
+# 画像移動
 def disp(screen, ball, curse):
   screen.blit(imgGround,[0,0])
   screen.blit(imgPitcher,[550,10])
@@ -27,26 +37,27 @@ def disp(screen, ball, curse):
   screen.blit(imgBall,[ball.x,ball.y]) 
 
 
+
 def main():
-  pygame.init()
+  test1.test1()
+
+
+  pygame.init() #pygame初期化
 
   ## 変数宣言
-  width = 1200 #画面横
-  height = 900 #画面縦
-  m = [width/2,100] #マウス(x,y)座標
-  curse = coordinate(width/2,100)
-  ball = coordinate(600,0)
 
-  pygame.display.set_caption("テスト")
-  screen = pygame.display.set_mode((width,height))
-  clock = pygame.time.Clock()
+  curse = coordinate(width/2,100) #マウス座標
+  ball = coordinate(600,0) #ボール座標
+
+  pygame.display.set_caption("テスト") #ウィンドウネーム指定
+  clock = pygame.time.Clock() #画面更新頻度
   
   pygame.mixer.init(frequency = 44100)    # 初期設定
   pygame.mixer.music.load("bgm/main.mid")     # 音楽ファイルの読み込み
-  pygame.mixer.music.play(-1)              # 音楽の再生回数(1回)
+  #pygame.mixer.music.play(-1)              # 音楽の再生回数(1回)
 
   #pygame.mouse.set_visible(False) # カーソル非表示（）
-  pygame.mouse.set_visible(True)
+  pygame.mouse.set_visible(True) 
   pygame.mouse.set_pos((curse.x,curse.y))  # カーソル初期位置
 
 
@@ -54,9 +65,15 @@ def main():
   owl_plus = True
   owl_count = [0,-5,5]
   owl_vari = 20
+  font = pygame.font.Font(None, 100)  #　文字フォント
+
+
 
   while True:
-    ball_y = disp(screen, ball, curse)
+    pygame.display.update()
+    clock.tick(60)
+
+    disp(screen, ball, curse)
 
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -70,8 +87,14 @@ def main():
           sys.exit()
 
     
-    pygame.display.update()
-    clock.tick(60)
+    
+
+    ## 当たり判定
+    #pygame.sprite.collide_rect()
+    ### 文字表示
+    text = font.render("HIT", True, (0,0,0))
+    screen.blit(text, [0, 0])# 文字列の表示位置
+
 
     ## 座標変更
     ### ボール
@@ -90,6 +113,7 @@ def main():
 
     print(curse.x,curse.y)
     print()
+
 
         
 if __name__ == "__main__":
